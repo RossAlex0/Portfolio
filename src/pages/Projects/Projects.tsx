93358df4319ea131f57e4da290121f619cafb071
Alex Rossignol <alexrossignol@macbookair.home>
2024-10-23 21:04:52 +0200

    Project page init

--- src/pages/Projects/Projects.tsx
+++ src/pages/Projects/Projects.tsx
@@ -1,3 +1,26 @@
+import CardProject from "../../components/CardProject/CardProject";
+import { ProjectInterface } from "../../components/CardProject/type";
+import projectsData from "../../services/data/projectsData.json";
+
+import "./projects.css";
+
 export default function Projects() {
-  return <p>Projects</p>;
+  const projects = projectsData as ProjectInterface[];
+
+  return (
+    <section className="project">
+      <header className="project_header">
+        <h1>Projects</h1>
+      </header>
+      <div className="project-buttons">
+        <button type="button">Mobile</button>
+        <button type="button">Web</button>
+      </div>
+      <div className="project_cards">
+        {projects.map((project) => (
+          <CardProject project={project} />
+        ))}
+      </div>
+    </section>
+  );
 }
