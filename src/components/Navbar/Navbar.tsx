7a2828bb9aa3b21886259bb23b3f86db358c0ccd
Alex Rossignol <alexrossignol@macbookair.home>
2024-10-24 15:18:28 +0200

    Updated condition for nav

--- src/components/Navbar/Navbar.tsx
+++ src/components/Navbar/Navbar.tsx
@@ -1,15 +1,30 @@
-import { Link } from "react-router-dom";
+import { Link, useLocation } from "react-router-dom";
 
 import "./navbar.css";
 
 export default function Navbar() {
+  const location = useLocation();
   return (
     <header className="navbar">
       <img src="/logo.svg" alt="AlexR" />
       <div className="navbar_navigation">
-        <Link to={"/"}>Home</Link>
-        <Link to={"/projects"}>Projects</Link>
-        <Link to={"/about"}>Who I am ?</Link>
+        <Link to={"/"} className={location.pathname === "/" ? "selected" : ""}>
+          Home
+        </Link>
+        <Link
+          to={"/projects"}
+          className={
+            location.pathname.startsWith("/projects") ? "selected" : ""
+          }
+        >
+          Projects
+        </Link>
+        <Link
+          to={"/about"}
+          className={location.pathname === "/about" ? "selected" : ""}
+        >
+          Who I am ?
+        </Link>
         <Link to={"/contact"}>
           <div>
             <p>Contact</p>
