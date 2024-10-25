e656de366447027424a77b0248c715388b05d3d4
Alex Rossignol <alexrossignol@macbookair.home>
2024-10-25 02:08:28 +0200

    Created contact page

--- src/pages/Contact/Contact.tsx
+++ src/pages/Contact/Contact.tsx
@@ -1,3 +1,35 @@
+import git from "/icon/github.svg";
+import cv from "/icon/cv.svg";
+import linkedin from "/icon/linkedin.svg";
+import "./contact.css";
+
 export default function Contact() {
-  return <p>Contact</p>;
+  return (
+    <section className="contact">
+      <div className="contact_header">
+        <h1>Contact</h1>
+        <div className="contact_header_link">
+          <div className="link_container">
+            <a href="https://github.com/RossAlex0" target="_blank">
+              <img src={git} alt="github" />
+            </a>
+          </div>
+          <div className="link_container link_down">
+            <a href="/CV.pdf" download="Rossignol_Alex_CV.pdf">
+              <img src={cv} alt="curriculomvitae" />
+            </a>
+          </div>
+          <div className="link_container">
+            <a
+              href="https://www.linkedin.com/in/rossignolalex/"
+              target="_blank"
+            >
+              <img src={linkedin} alt="linkedin" />
+            </a>
+          </div>
+        </div>
+      </div>
+      <div className="body_contact"></div>
+    </section>
+  );
 }
