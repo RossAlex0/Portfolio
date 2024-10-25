9e22338ddc615e6111590d04b874a473132df440
Alex Rossignol <alexrossignol@macbookair.home>
2024-10-25 22:55:48 +0200

    New project structure racine

--- src/pages/About/type.ts
+++
@@ -1,5 +0,0 @@
-export interface SkillInterface {
-  softs: string[];
-  hards: string[];
-  tools: string[];
-}
--- src/services/utils/filterFormat.ts
+++
@@ -1,8 +0,0 @@
-import { ProjectInterface } from "../../components/CardProject/type";
-import projectsData from "../data/projectsData.json";
-
-const projects = projectsData as ProjectInterface[];
-
-export const getProjectsFormat = (format: string): ProjectInterface[] => {
-  return projects.filter((project) => project.format === format);
-};
