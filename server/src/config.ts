f24d4c7c8c0fbc325548c2587ba120816dbef9d2
Alex Rossignol <alexrossignol@macbookair.home>
2024-10-26 21:55:26 +0200

    Cors in config

--- server/src/config.ts
+++ server/src/config.ts
@@ -1,10 +1,17 @@
-import express, { Request, Response } from "express";
+import express from "express";
+import cors from "cors";
 
 import router from "./router/router";
 
 const app = express();
 
 app.use(express.json());
+
+app.use(
+  cors({
+    origin: process.env.APP_CLIENT_URL,
+  })
+);
 
 app.use("/api", router);
 
