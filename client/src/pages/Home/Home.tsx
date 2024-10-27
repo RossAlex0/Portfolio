d09a3958a7e6d997139cf6d650cb8fcfd6eb4169
Alex Rossignol <alexrossignol@macbookair.home>
2024-10-27 17:07:13 +0100

    Fr language

--- client/src/pages/Home/Home.tsx
+++ client/src/pages/Home/Home.tsx
@@ -3,17 +3,18 @@ import "./home.css";
 export default function Home() {
   return (
     <section className="home">
-      <div className="home_name">
-        <div className="animationContainer_name">
-          <p>Hello I'm Alex</p>
-          <p>,</p>
+      <div>
+        <div className="home_name">
+          <div className="animationContainer_name">
+            <p>Salut, moi c'est Alex</p>
+          </div>
         </div>
-      </div>
-      <div className="home_creative">
-        <div className="animationContainer_creative">
-          <p> a </p>
-          <p> creative </p>
-          <p> developer.</p>
+        <div className="home_creative">
+          <div className="animationContainer_creative">
+            <p> un </p>
+            <p> developpeur </p>
+            <p> creatif.</p>
+          </div>
         </div>
       </div>
     </section>
