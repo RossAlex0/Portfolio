5f33e8d0de513f6363eeec0e554e633dc35a91b7
Alex Rossignol <alexrossignol@macbookair.home>
2024-10-29 23:53:46 +0100

    Created component for link social btn

---
+++ client/src/components/ButtonLink/ButtonLink.tsx
@@ -0,0 +1,27 @@
+import git from "/icon/github.svg";
+import cv from "/icon/cv.svg";
+import linkedin from "/icon/linkedin.svg";
+
+import "./buttonLink.css";
+
+export default function ButtonLink() {
+  return (
+    <div className="contact_header_link">
+      <div className="link_container">
+        <a href="https://github.com/RossAlex0" target="_blank">
+          <img src={git} alt="github" />
+        </a>
+      </div>
+      <div className="link_container link_down">
+        <a href="/CV.pdf" download="Rossignol_Alex_CV.pdf">
+          <img src={cv} alt="curriculomvitae" />
+        </a>
+      </div>
+      <div className="link_container">
+        <a href="https://www.linkedin.com/in/rossignolalex/" target="_blank">
+          <img src={linkedin} alt="linkedin" />
+        </a>
+      </div>
+    </div>
+  );
+}
