8e8bd46e656abcd13c62a8ec7843c18ce877a8f4
Alex Rossignol <alexrossignol@macbookair.home>
2024-10-30 22:49:36 +0100

    Config deploy

--- api/src/index.ts
+++ api/src/index.ts
@@ -4,7 +4,7 @@ import config from "./config";
 
 const app = config;
 
-const PORT = process.env.APP_PORT;
+const PORT = process.env.PORT;
 
 app.listen(PORT, () => {
   console.log(`Server is running on http://localhost:${PORT}`);
