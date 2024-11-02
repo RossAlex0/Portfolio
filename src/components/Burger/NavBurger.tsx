eb0834afd280e7836330341dfe284bfbf35138bd
Alex Rossignol <alexrossignol@macbookair.home>
2024-11-02 01:14:02 +0100

    Translate mobile

--- src/components/Burger/NavBurger.tsx
+++ src/components/Burger/NavBurger.tsx
@@ -1,14 +1,34 @@
 import { Link, useLocation } from "react-router-dom";
-import { useState } from "react";
+import { useContext, useState } from "react";
+
+import translate from "../../services/data/translate.json";
+import {
+  LanguageContext,
+  LanguageContextInterface,
+} from "../../services/context/languageContext";
+import { NavLanguage } from "../Navbar/type";
 
 import ButtonLink from "../ButtonLink/ButtonLink";
 
 import "./burger.css";
 
 export default function NavBurger() {
+  const { language, setLanguage } = useContext(
+    LanguageContext
+  ) as LanguageContextInterface;
+
+  const t: NavLanguage = translate.navbar;
   const location = useLocation();
 
   const [isActive, setIsActive] = useState<boolean>(false);
+
+  const handleClickToggle = () => {
+    if (language === "fr") {
+      setLanguage("en");
+    } else {
+      setLanguage("fr");
+    }
+  };
 
   return (
     <header className="burger">
@@ -38,7 +58,7 @@ export default function NavBurger() {
                 onClick={() => setIsActive(false)}
                 className={location.pathname === "/" ? "selected" : ""}
               >
-                Accueil
+                {t.accueil[language]}
               </Link>
             </li>
             <li>
@@ -49,7 +69,7 @@ export default function NavBurger() {
                   location.pathname.startsWith("/projects") ? "selected" : ""
                 }
               >
-                Projets
+                {t.projets[language]}
               </Link>
             </li>
             <li>
@@ -58,7 +78,7 @@ export default function NavBurger() {
                 onClick={() => setIsActive(false)}
                 className={location.pathname === "/about" ? "selected" : ""}
               >
-                Qui suis-je ?
+                {t.about[language]}
               </Link>
             </li>
             <li>
@@ -70,7 +90,18 @@ export default function NavBurger() {
                 Contact
               </Link>
             </li>
+            <li>
+              <div className="toggle">
+                <p className="fr">Fr</p>
+                <label className="switch_mobile">
+                  <input type="checkbox" onClick={handleClickToggle} />
+                  <span className="slider_mobile"></span>
+                </label>
+                <p className="en">En</p>
+              </div>
+            </li>
           </ul>
+
           <div className="burger_link">
             <ButtonLink />
           </div>
--- src/components/CardProject/CardProject.tsx
+++ src/components/CardProject/CardProject.tsx
@@ -1,15 +1,20 @@
 import { ProjectPropsInterface } from "./type";
 import { useNavigate } from "react-router-dom";
-import { useEffect, useState } from "react";
+import { useContext, useEffect, useState } from "react";
 
+import useScreenWidth from "../../services/hook/useScreenWodth";
+import {
+  LanguageContext,
+  LanguageContextInterface,
+} from "../../services/context/languageContext";
 import { getProjectsFormat } from "../../services/utils/filterFormat";
 
 import "./CardProject.css";
-import useScreenWidth from "../../services/hook/useScreenWodth";
 
 export default function CardProject({ project }: ProjectPropsInterface) {
   const navigate = useNavigate();
   const screenWidth = useScreenWidth();
+  const { language } = useContext(LanguageContext) as LanguageContextInterface;
 
   const [index, setIndex] = useState<number>();
 
@@ -33,22 +38,42 @@ export default function CardProject({ project }: ProjectPropsInterface) {
         <h2>{project.name}</h2>
         <button
           type="button"
+          className={`button_card_${language}`}
           onClick={() => navigate(`/projects/${project.format}/${index}`)}
         >
-          <i>V</i>
-          <i>o</i>
-          <i>i</i>
-          <i>r</i>
-          <i>&nbsp;</i>
-          <i>l</i>
-          <i>e</i>
-          <i>&nbsp;</i>
-          <i>p</i>
-          <i>r</i>
-          <i>o</i>
-          <i>j</i>
-          <i>e</i>
-          <i>t</i>
+          {language === "fr" ? (
+            <>
+              <i>V</i>
+              <i>o</i>
+              <i>i</i>
+              <i>r</i>
+              <i>&nbsp;</i>
+              <i>l</i>
+              <i>e</i>
+              <i>&nbsp;</i>
+              <i>p</i>
+              <i>r</i>
+              <i>o</i>
+              <i>j</i>
+              <i>e</i>
+              <i>t</i>
+            </>
+          ) : (
+            <>
+              <i>V</i>
+              <i>i</i>
+              <i>e</i>
+              <i>w</i>
+              <i>&nbsp;</i>
+              <i>p</i>
+              <i>r</i>
+              <i>o</i>
+              <i>j</i>
+              <i>e</i>
+              <i>c</i>
+              <i>t</i>
+            </>
+          )}
         </button>
       </div>
     </div>
