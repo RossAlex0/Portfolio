28d561f933a96a3ea30e1fbb1937b125240bd1b9
Alex Rossignol <alexrossignol@macbookair.home>
2024-11-02 01:53:33 +0100

    Logique for transalte into storage

--- src/components/Burger/NavBurger.tsx
+++ src/components/Burger/NavBurger.tsx
@@ -31,82 +31,84 @@ export default function NavBurger() {
   };
 
   return (
-    <header className="burger">
-      <img src="/logo.svg" alt="AlexR" />
+    language && (
+      <header className="burger">
+        <img src="/logo.svg" alt="AlexR" />
 
-      <button
-        type="button"
-        className="burger_btn"
-        onClick={() => setIsActive(!isActive)}
-      >
-        <div className={isActive ? "bar1 active_bar1" : "bar1"}></div>
-        <div className={isActive ? "bar2 active_bar2" : "bar2"}></div>
-        <div className={isActive ? "bar3_h active_bar3_h" : "bar3_h"}></div>
-        <div className={isActive ? "bar4 active_bar4" : "bar4"}></div>
-      </button>
-
-      <div className={isActive ? "burger_list active" : "burger_list"}>
-        <div
-          className={
-            isActive ? "burger_list_element active" : "burger_list_element"
-          }
+        <button
+          type="button"
+          className="burger_btn"
+          onClick={() => setIsActive(!isActive)}
         >
-          <ul>
-            <li>
-              <Link
-                to={"/"}
-                onClick={() => setIsActive(false)}
-                className={location.pathname === "/" ? "selected" : ""}
-              >
-                {t.accueil[language]}
-              </Link>
-            </li>
-            <li>
-              <Link
-                to={"/projects"}
-                onClick={() => setIsActive(false)}
-                className={
-                  location.pathname.startsWith("/projects") ? "selected" : ""
-                }
-              >
-                {t.projets[language]}
-              </Link>
-            </li>
-            <li>
-              <Link
-                to={"/about"}
-                onClick={() => setIsActive(false)}
-                className={location.pathname === "/about" ? "selected" : ""}
-              >
-                {t.about[language]}
-              </Link>
-            </li>
-            <li>
-              <Link
-                to={"/contact"}
-                onClick={() => setIsActive(false)}
-                className={location.pathname === "/contact" ? "selected" : ""}
-              >
-                Contact
-              </Link>
-            </li>
-            <li>
-              <div className="toggle">
-                <p className="fr">Fr</p>
-                <label className="switch_mobile">
-                  <input type="checkbox" onClick={handleClickToggle} />
-                  <span className="slider_mobile"></span>
-                </label>
-                <p className="en">En</p>
-              </div>
-            </li>
-          </ul>
+          <div className={isActive ? "bar1 active_bar1" : "bar1"}></div>
+          <div className={isActive ? "bar2 active_bar2" : "bar2"}></div>
+          <div className={isActive ? "bar3_h active_bar3_h" : "bar3_h"}></div>
+          <div className={isActive ? "bar4 active_bar4" : "bar4"}></div>
+        </button>
 
-          <div className="burger_link">
-            <ButtonLink />
+        <div className={isActive ? "burger_list active" : "burger_list"}>
+          <div
+            className={
+              isActive ? "burger_list_element active" : "burger_list_element"
+            }
+          >
+            <ul>
+              <li>
+                <Link
+                  to={"/"}
+                  onClick={() => setIsActive(false)}
+                  className={location.pathname === "/" ? "selected" : ""}
+                >
+                  {t.accueil[language]}
+                </Link>
+              </li>
+              <li>
+                <Link
+                  to={"/projects"}
+                  onClick={() => setIsActive(false)}
+                  className={
+                    location.pathname.startsWith("/projects") ? "selected" : ""
+                  }
+                >
+                  {t.projets[language]}
+                </Link>
+              </li>
+              <li>
+                <Link
+                  to={"/about"}
+                  onClick={() => setIsActive(false)}
+                  className={location.pathname === "/about" ? "selected" : ""}
+                >
+                  {t.about[language]}
+                </Link>
+              </li>
+              <li>
+                <Link
+                  to={"/contact"}
+                  onClick={() => setIsActive(false)}
+                  className={location.pathname === "/contact" ? "selected" : ""}
+                >
+                  Contact
+                </Link>
+              </li>
+              <li>
+                <div className="toggle">
+                  <p className="fr">Fr</p>
+                  <label className="switch_mobile">
+                    <input type="checkbox" onClick={handleClickToggle} />
+                    <span className="slider_mobile"></span>
+                  </label>
+                  <p className="en">En</p>
+                </div>
+              </li>
+            </ul>
+
+            <div className="burger_link">
+              <ButtonLink />
+            </div>
           </div>
         </div>
-      </div>
-    </header>
+      </header>
+    )
   );
 }
--- src/pages/Projects/Projects.tsx
+++ src/pages/Projects/Projects.tsx
@@ -43,104 +43,108 @@ export default function Projects() {
   };
 
   return (
-    <section className="projects">
-      <header className="project_header">
-        <h1>{t.title[language]}</h1>
-      </header>
-      <div className="project-buttons">
-        <button
-          type="button"
-          className="button_switch"
-          onClick={() => setProjectFilter(getProjectsFormat("mobile"))}
-        >
-          Mobile
-        </button>
-        <button
-          type="button"
-          className="button_switch"
-          onClick={() => setProjectFilter(getProjectsFormat("web"))}
-        >
-          Web
-        </button>
-      </div>
-      <div
-        className={screenWidth < 900 ? "project_cards_format" : "project_cards"}
-      >
-        {screenWidth < 900 ? (
-          isProject.map((project) => (
-            <>
-              <CardProject project={project} />{" "}
-            </>
-          ))
-        ) : (
-          <Swiper
-            modules={[EffectCoverflow, Parallax, Navigation]}
-            effect="coverflow"
-            centeredSlides={true}
-            slidesPerView={3} // Utilisation de "auto" pour un centrage dynamique
-            spaceBetween={30} // Ajuste l’espace entre les slides pour réduire les chevauchements
-            parallax={true}
-            navigation
-            coverflowEffect={{
-              rotate: 30,
-              stretch: 0,
-              depth: 200,
-              modifier: 1,
-              slideShadows: true,
-            }}
-            loop={true}
-            speed={800}
+    language && (
+      <section className="projects">
+        <header className="project_header">
+          <h1>{t.title[language]}</h1>
+        </header>
+        <div className="project-buttons">
+          <button
+            type="button"
+            className="button_switch"
+            onClick={() => setProjectFilter(getProjectsFormat("mobile"))}
           >
-            {isProject.map((project) => (
-              <SwiperSlide key={project.id}>
-                <h3 className="title_project">{project.name}</h3>
-                <img src={project.image} alt={project.name} />
-                <div className="button_project_container">
-                  <button
-                    type="button"
-                    className={`button_project_${language}`}
-                    onClick={() => handleNavigate(project)}
-                  >
-                    {language === "fr" ? (
-                      <>
-                        <i>V</i>
-                        <i>o</i>
-                        <i>i</i>
-                        <i>r</i>
-                        <i>&nbsp;</i>
-                        <i>l</i>
-                        <i>e</i>
-                        <i>&nbsp;</i>
-                        <i>p</i>
-                        <i>r</i>
-                        <i>o</i>
-                        <i>j</i>
-                        <i>e</i>
-                        <i>t</i>
-                      </>
-                    ) : (
-                      <>
-                        <i>V</i>
-                        <i>i</i>
-                        <i>e</i>
-                        <i>w</i>
-                        <i>&nbsp;</i>
-                        <i>p</i>
-                        <i>r</i>
-                        <i>o</i>
-                        <i>j</i>
-                        <i>e</i>
-                        <i>c</i>
-                        <i>t</i>
-                      </>
-                    )}
-                  </button>
-                </div>
-              </SwiperSlide>
-            ))}
-          </Swiper>
-        )}
-      </div>
-    </section>
+            Mobile
+          </button>
+          <button
+            type="button"
+            className="button_switch"
+            onClick={() => setProjectFilter(getProjectsFormat("web"))}
+          >
+            Web
+          </button>
+        </div>
+        <div
+          className={
+            screenWidth < 900 ? "project_cards_format" : "project_cards"
+          }
+        >
+          {screenWidth < 900 ? (
+            isProject.map((project) => (
+              <>
+                <CardProject project={project} />{" "}
+              </>
+            ))
+          ) : (
+            <Swiper
+              modules={[EffectCoverflow, Parallax, Navigation]}
+              effect="coverflow"
+              centeredSlides={true}
+              slidesPerView={3} // Utilisation de "auto" pour un centrage dynamique
+              spaceBetween={30} // Ajuste l’espace entre les slides pour réduire les chevauchements
+              parallax={true}
+              navigation
+              coverflowEffect={{
+                rotate: 30,
+                stretch: 0,
+                depth: 200,
+                modifier: 1,
+                slideShadows: true,
+              }}
+              loop={true}
+              speed={800}
+            >
+              {isProject.map((project) => (
+                <SwiperSlide key={project.id}>
+                  <h3 className="title_project">{project.name}</h3>
+                  <img src={project.image} alt={project.name} />
+                  <div className="button_project_container">
+                    <button
+                      type="button"
+                      className={`button_project_${language}`}
+                      onClick={() => handleNavigate(project)}
+                    >
+                      {language === "fr" ? (
+                        <>
+                          <i>V</i>
+                          <i>o</i>
+                          <i>i</i>
+                          <i>r</i>
+                          <i>&nbsp;</i>
+                          <i>l</i>
+                          <i>e</i>
+                          <i>&nbsp;</i>
+                          <i>p</i>
+                          <i>r</i>
+                          <i>o</i>
+                          <i>j</i>
+                          <i>e</i>
+                          <i>t</i>
+                        </>
+                      ) : (
+                        <>
+                          <i>V</i>
+                          <i>i</i>
+                          <i>e</i>
+                          <i>w</i>
+                          <i>&nbsp;</i>
+                          <i>p</i>
+                          <i>r</i>
+                          <i>o</i>
+                          <i>j</i>
+                          <i>e</i>
+                          <i>c</i>
+                          <i>t</i>
+                        </>
+                      )}
+                    </button>
+                  </div>
+                </SwiperSlide>
+              ))}
+            </Swiper>
+          )}
+        </div>
+      </section>
+    )
   );
 }
